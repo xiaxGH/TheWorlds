e776295cea6352a018131bce3d39c9fa68b2a61e
Baek Young Hoon <yyh0965@gmail.com>
2023-05-16 20:53:25 +0900

    버튼 클릭 시 일시정지 하는중

---
+++ Assets/Script/TimeStop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TImeStop : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // 게임 정지
+        }
+        else
+        {
+            Time.timeScale = 1f; // 게임 재개
+        }
+    }
+}
