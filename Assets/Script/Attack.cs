3b0faa88c550346ac063197b91364950fadbfbf2
박도현 <qwert97055@gmail.com>
2023-05-16 19:45:22 +0900

    공격 애니메이팅 중
    ㅁㄴㅇㄹ

--- Assets/Script/Attack.cs
+++ Assets/Script/Attack.cs
@@ -9,6 +9,11 @@ public class Attack : MonoBehaviour
     Transform parentTransform;
     private Vector3 lastPosition;
 
+    public string SwordAtk = "sword";
+
+    string nowAnimation = "";
+    string oldAnimation = "";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,13 +34,15 @@ public class Attack : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<CapsuleCollider2D>().enabled = true;   // Collider 컴포넌트 활성화
-            renderer.flipY = true;
+            nowAnimation = SwordAtk;
+            GetComponent<Animator>().Play(SwordAtk);
+            //renderer.flipY = true;
         }
         // 스페이스바의 입력이 해제 됐을 때 실행되는 코드
         if (Input.GetKeyUp(KeyCode.Space))
         {
             GetComponent<CapsuleCollider2D>().enabled = false;  // Collider 컴포넌트 비활성화
-            renderer.flipY = false;
+            //renderer.flipY = false;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
