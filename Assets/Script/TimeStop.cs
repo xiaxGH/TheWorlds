94caec500747ffde46fa4f619603c469461fdde6
박도현 <qwert97055@gmail.com>
2023-05-22 20:15:52 +0900

    0.1.0
    통합본 중간

--- Assets/Script/TimeStop.cs
+++ Assets/Script/TimeStop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TImeStop : MonoBehaviour
+public class TimeStop : MonoBehaviour
 {
     private bool isPaused = false;
 
